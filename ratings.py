460d68d80aec17f540fa9f047d0ba08932a0ad85
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-12-09 09:26:45 -0500

    Read, stored, sorted, and printed ratings

--- ratings.py
+++ ratings.py
@@ -1,3 +1,18 @@
 """Restaurant rating lister."""
 
+ratings = {}
+
 file = open('./scores.txt', 'r')
+
+for line in file:
+    line = line.strip('\n')
+    (key, value) = line.split(':')
+    ratings[key] = value
+
+sorted_ratings = sorted(ratings.items())
+
+for tuple in sorted_ratings:
+    print(f'{tuple[0]} is rated at {tuple[1]}.')
+
+
+file.close()
