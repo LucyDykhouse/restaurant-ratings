833fc61143e13c06035a80c8997665723f709e5e
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-12-09 09:38:51 -0500

    Added capability of user to enter a rating

--- ratings.py
+++ ratings.py
@@ -1,18 +1,27 @@
 """Restaurant rating lister."""
 
+# Create dictionary to store restaurants/ratings
 ratings = {}
 
+# Open the text file
 file = open('./scores.txt', 'r')
 
+# Read in the text file contents and add to dictionary
 for line in file:
     line = line.strip('\n')
-    (key, value) = line.split(':')
-    ratings[key] = value
+    key, value = line.split(':')
+    ratings[key] = int(value)
 
-sorted_ratings = sorted(ratings.items())
+# Prompt the user for a new restaurant/rating
+new_rest = input('Enter a restaurant name: ')
+new_rating = input('Enter rating of ' + new_rest + ': ')
 
-for tuple in sorted_ratings:
-    print(f'{tuple[0]} is rated at {tuple[1]}.')
+# Insert new restaurant/rating into dictionary
+ratings[new_rest] = new_rating
 
+# Print the ratings in alphabetical order
+for restaurant, rating in sorted(ratings.items()):
+    print(f'{restaurant} is rated at {rating}.')
 
+# Close the file
 file.close()
